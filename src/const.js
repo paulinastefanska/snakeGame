5ec067840712c1c692ca896a182c71a9567f7a15
Paulina Stefanska <web.paulinastefanska@gmail.com>
2020-01-04 19:15:20 +0100

    Add config

---
+++ src/const.js
@@ -0,0 +1,8 @@
+export const ROWS = 20,
+  COLS = 20,
+  CELL = 30;
+export const BODY = 1,
+  FOOD = 2;
+export const START = 44;
+export const KEYS = { left: 37, up: 38, right: 39, down: 40 };
+export const DIRS = { 37: true, 38: true, 39: true, 40: true };
