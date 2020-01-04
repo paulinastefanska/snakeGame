6260637ffa14952433201e36717d95632ed5637d
Paulina Stefanska <web.paulinastefanska@gmail.com>
2020-01-04 19:26:08 +0100

    Updates

---
+++ precache-manifest.965dd339f01f3fadf5d91a46d1b4d4bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f3dafa503b47af403d163353da8adac",
+    "url": "/snakeGame/index.html"
+  },
+  {
+    "revision": "ede4e5ded3d1291d1ba1",
+    "url": "/snakeGame/static/css/main.72c59538.chunk.css"
+  },
+  {
+    "revision": "e99a232a35d7f2989968",
+    "url": "/snakeGame/static/js/2.ccdbf324.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/snakeGame/static/js/2.ccdbf324.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ede4e5ded3d1291d1ba1",
+    "url": "/snakeGame/static/js/main.296a1162.chunk.js"
+  },
+  {
+    "revision": "4cb1e245b0845661c7df",
+    "url": "/snakeGame/static/js/runtime-main.7ec2cecc.js"
+  }
+]);
